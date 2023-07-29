d64558dbcd00a90b342dea75cc0cfc168403389a
Hayat Singh <hayat.singh@olacabs.com>
2023-07-29 17:16:22 +0530

    QUES ADDED

---
+++ array.js
@@ -0,0 +1,8 @@
+let arr = ['hayat', 'singh'];
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  for (let j = 0; j <= arr.length - 1; j++) {
+    console.log('first', arr[i]);
+    console.log('sec', arr[j]);
+  }
+}
