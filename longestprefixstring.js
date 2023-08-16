e21719d2dc436918e94f534a01ac9a70124e4acb
hayat <jeet53316@gmail.com>
2023-08-16 23:27:28 +0530

    programs added

---
+++ longestprefixstring.js
@@ -0,0 +1,7 @@
+let arr = ["flower","flow","flight"];
+for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr[i].length;j++){
+        // console.log("every word in array",arr[i][j])
+    }
+    // console.log("every element in array",arr[i])
+}
