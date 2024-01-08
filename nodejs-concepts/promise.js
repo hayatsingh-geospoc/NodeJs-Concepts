f71dcc05d33235fe407093073e6780363fb5abf3
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-08 23:48:54 +0530

    promises added

--- nodejs-concepts/promise.js
+++ nodejs-concepts/promise.js
@@ -1,21 +1,3 @@
-let arr = [1,10,4,5,9,2,6];
-let data = [];
-for(let i=0;i<arr.length;i++){
-    if(arr[i]>arr[i+1]){
-        let temp = arr[i];
-        arr[i]=arr[i+1];
-        arr[i+1]=temp;
-        i=-1
-    }
-    // console.log("arr[i]",arr[i]);
-    // console.log("arr[i+1]",arr[i+1])}
-}
-
-for(let j=0;j<arr.length;j++){
-    if((arr[j+1]-arr[j])%2==0){
-        data.push(arr[j])
-    }
-}
-
-console.log(data)
-console.log(arr)
+let prom = new Promise((req,res)=>{
+    
+})
