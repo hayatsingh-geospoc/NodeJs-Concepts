99a3cc343dd7a9c61369eadeda61d0a0552bf751
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-17 23:40:43 +0530

    arr-obj concepts

---
+++ nodejs-concepts/arr-obj.js
@@ -0,0 +1,41 @@
+// Creating an array of objects with nested data
+const arrayOfObjects = [
+    {
+      id: 1,
+      name: 'John Doe',
+      address: {
+        street: '123 Main St',
+        city: 'Cityville',
+        zip: 3
+      }
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      address: {
+        street: '456 Oak Ave',
+        city: 'Townsville',
+        zip: 6
+      }
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      address: {
+        street: '789 Pine Ln',
+        city: 'Villageville',
+        zip: 8
+      }
+    }
+  ];
+  
+
+
+
+let data = arrayOfObjects.map((datas,index)=> {
+
+     return datas.address.zip > 5
+
+})
+
+console.log(data)
